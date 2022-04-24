83ffb9c2d8ff804aa90bf64c3083091b6a14a804
江浩 <245129129@qq.com>
2022-04-24 16:17:12 +0800

    Update filehelper.py
    默认关闭代理

--- filehelper.py
+++ filehelper.py
@@ -342,8 +342,8 @@ class WXRequest:
 
     def fetch(self, url, method="get", params=None, data=None, json=None, timeout=10):
         resp = self.session.request(
-            method, url, params=params, data=data, json=json, timeout=timeout, allow_redirects=False, verify=False, proxies={'https': 'http://127.0.0.1:8888'})
-        # method, url, params=params, data=data, json=json, timeout=timeout, allow_redirects=False)
+            # method, url, params=params, data=data, json=json, timeout=timeout, allow_redirects=False, verify=False, proxies={'https': 'http://127.0.0.1:8888'})
+            method, url, params=params, data=data, json=json, timeout=timeout, allow_redirects=False)
         if resp and resp.status_code == requests.codes.ok:
             return resp
         else:
